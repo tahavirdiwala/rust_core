dbad3f21723e265e2bc8ae290d3c4b87cc2f5bd6
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-08-05 09:06:24 +0530

    ...

--- src/main.rs
+++ src/main.rs
@@ -319,3 +319,14 @@ struct Rectangle {
 
 //     println!("{:#?} {:#?}", home, back)
 // }
+
+// fn main() {
+//     let x: i8 = 5;
+//     let y: Option<i8> = Some(5);
+
+//     let sum = x + y;
+
+//     println!("{}", sum);
+// }
+
+//control flow
